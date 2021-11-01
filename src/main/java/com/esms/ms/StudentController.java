be54ec8c5bae606d3773f97d3f311fbea6d70dff
aredd23 <adireddy.gr@gmail.com>
2021-11-01 13:27:29 +0530

    initial commit

---
+++ src/main/java/com/esms/ms/StudentController.java
@@ -0,0 +1,21 @@
+package com.esms.ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/student")
+@RestController
+public class StudentController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+	
+	@GetMapping("/azure")
+	public String indexAzure() {
+		return "Greetings from Azure-Spring Boot!";
+	}
+
+}
