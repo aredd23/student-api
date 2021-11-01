bbb218137f03b7fcd366fd36d3233406a8e11efe
aredd23 <adireddy.gr@gmail.com>
2021-11-01 16:52:33 +0530

    Update UserController.java

--- src/main/java/com/esms/ms/UserController.java
+++ src/main/java/com/esms/ms/UserController.java
@@ -1,6 +1,11 @@
 package com.esms.ms;
 
+import java.util.Optional;
+
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,5 +29,17 @@ public class UserController {
 	@GetMapping(path = "/all")
 	public @ResponseBody Iterable<User> getAllUsers() {
 		return userRepository.findAll();
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public String deleteUsers(@PathParam(value = "id") Integer id) {
+		userRepository.deleteById(id);
+		
+		return "deleted user id"+id;
+	}
+	
+	@GetMapping(path = "/{id}")
+	public @ResponseBody Optional<User> getUser(@PathParam(value = "id") Integer id) {
+		return userRepository.findById(id);
 	}
 }
