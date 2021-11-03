8c0b8d5222951cffcd87a276846502c645b1c8c2
aredd23 <adireddy.gr@gmail.com>
2021-11-03 09:11:29 +0530

    Update UserController.java

--- src/main/java/com/esms/ms/UserController.java
+++ src/main/java/com/esms/ms/UserController.java
@@ -2,12 +2,13 @@ package com.esms.ms;
 
 import java.util.Optional;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,6 +25,20 @@ public class UserController {
 		User n = new User();
 		n.setName(name);
 		userRepository.save(n);
+		return "Saved";
+	}
+	
+	@PostMapping(path = "/")
+	public @ResponseBody String addUser(@RequestBody User user) {
+		
+		userRepository.save(user);
+		return "Saved";
+	}
+	
+	@PutMapping(path = "/{id}")
+	public @ResponseBody String updateUser(@RequestBody User user,@PathVariable Integer id) {
+		user.setId(id);
+		userRepository.save(user);
 		return "Saved";
 	}
 
